eaf7010d4cf88343765da48a5bb1d7e2a41fb5a3
Han <hanseonuk@naver.com>
2024-06-10 07:27:41 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,13 +7,20 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    if(cnt==M){
+	for(int i=0;i<M;i++){
+	    if(i>0) cout << " ";
+	    cout << vec[i];
+	}
+	cout << "\n";
+	return;
+    }
+    int start = (vec.empty()) ? 1:vec.back()+1;
+    for(int i=start;i<=N;++i){
+	vec.push_back(i);
+	func(cnt+1);
+	vec.pop_back();
+    }
 }
 
 int main() {
